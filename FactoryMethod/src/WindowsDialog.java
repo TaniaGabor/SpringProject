6a7faad19ab857e5c819de2dbef247d50cf16d28
Tania Gabor <taniagabor@yahoo.com>
2020-10-07 14:49:51 +0300

    .

---
+++ FactoryMethod/src/WindowsDialog.java
@@ -0,0 +1,13 @@
+
+
+
+/**
+ * Windows Dialog will produce Windows buttons.
+ */
+public class WindowsDialog extends Dialog {
+
+    @Override
+    public Button createButton() {
+        return new WindowsButton();
+    }
+}
